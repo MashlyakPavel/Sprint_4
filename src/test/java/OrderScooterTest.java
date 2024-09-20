e2b2813a3b3fd671080a819c7149b787be5af476
Pavel Mashlyak <mpainfin@gmail.com>
2024-09-20 16:26:16 +0300

    rename README

--- src/test/java/OrderScooterTest.java
+++ src/test/java/OrderScooterTest.java
@@ -1,45 +1,87 @@
-import org.junit.After;
-import org.junit.Assert;
+iimport org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import pageobjects.MainPage;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pageobjects.PageOrder;
 
-public class OrderScooterTests {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class OrderScooterTest {
+
+    private WebDriver driver;
     private WebDriver driver;
     private MainPage mainPage;
+    // Параметры теста
+    @Parameterized.Parameter(0)
+    public String name;
+    @Parameterized.Parameter(1)
+    public String surname;
+    @Parameterized.Parameter(2)
+    public String address;
+    @Parameterized.Parameter(3)
+    public int metro;
+    @Parameterized.Parameter(4)
+    public String phoneNumber;
+    @Parameterized.Parameter(5)
+    public String color;
+    @Parameterized.Parameter(6)
+    public String date;
+    @Parameterized.Parameter(7)
+    public int rentalDays;
+    @Parameterized.Parameter(8)
+    public String comment;
 
     @Before
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("https://qa-scooter.praktikum-services.ru/");
         mainPage = new MainPage(driver);
     }
+    // Закрытие баннера с cookie, если он есть
+        pageOrder.closeCookieBanner();
+}
 
-    @Test
-    public void testOrderScooterFromTopButton() {
-        mainPage.clickOrderButtonTop();
-        mainPage.fillOrderForm("Иван", "Иванов", "ул. Ленина, д. 1, кв. 1", "Чистые пруды", "+79991234567");
-        Assert.assertTrue(mainPage.isSuccessModalDisplayed());
-    }
+// Нажатие на верхнюю кнопку "Заказать"
+@Test
+public void testOrderScooterThroughHeaderButton() {
+    pageOrder.clickOrderButtonHeader();
+    pageOrder.enterDataFirstPageOrder(name, surname, address, metro, phoneNumber);
+    pageOrder.enterDataSecondPageOrder(color, date, rentalDays, comment);
+    assertTrue(pageOrder.successfullyText());
+}
 
-    @Test
-    public void testOrderScooterFromBottomButton() {
-        mainPage.clickOrderButtonBottom();
-        mainPage.fillOrderForm("Анна", "Петрова", "ул. Гагарина, д. 2, кв. 2", "Павелецкая", "+79997654321");
-        Assert.assertTrue(mainPage.isSuccessModalDisplayed());
-    }
+//для заказа по нижней кнопке:
+@Test
+public void testOrderScooterThroughDownButton() {
+    pageOrder.scrollPageOrder(); // Скролим страницу
+    pageOrder.clickOrderButtonDown(); // Нажатие на нижнюю кнопку "Заказать"
+    pageOrder.enterDataFirstPageOrder(name, surname, address, metro, phoneNumber);
+    pageOrder.enterDataSecondPageOrder(color, date, rentalDays, comment);
+    assertTrue(pageOrder.successfullyText());
+}
 
-    @Test
-    public void testFaqTextToggle() {
-        mainPage.toggleFaqText();
-        Assert.assertTrue(!mainPage.getFaqText().isEmpty());
-    }
-
-    @After
-    public void teardown() {
-        driver.quit();
-    }
+@After
+public void teardown() {
+    driver.quit();
+}
+}
+@Parameterized.Parameters
+public static Collection<Object[]> testData() {
+    return Arrays.asList(new Object[][]{
+            {"Анна", "Петрова", "ул.Гагарина, д. 2, кв.2" , "Павелецкая", "+79997654321"},
+            {"Иван", "Иванов", "ул.Ленина, д. 1, кв.1" , "Чистые пруды", "+79991234567"}
+    });
+}
 }
